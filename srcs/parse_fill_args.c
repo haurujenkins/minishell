21817d319cd1707d0da7c6e9c09297d255939571
Axel Bolea <abolea@z1r3p2.42lyon.fr>
2024-05-27 16:38:40 +0200

    merge

--- srcs/parse_fill_args.c
+++ srcs/parse_fill_args.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_fill_args.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lle-pier <lle-pier@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: abolea <abolea@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 15:37:45 by lle-pier          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/05/27 16:29:45 by abolea           ###   ########.fr       */
-=======
-/*   Updated: 2024/05/24 16:42:17 by lle-pier         ###   ########.fr       */
->>>>>>> main
+/*   Updated: 2024/05/27 16:38:33 by abolea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -201,11 +197,7 @@ char	*sup_dollar_before_quotes(char *s)
 		}
 	}
 	res[j] = '\0';
-<<<<<<< HEAD
 	free (s);
-=======
-	free(s);
->>>>>>> main
 	return (res);
 }
 
