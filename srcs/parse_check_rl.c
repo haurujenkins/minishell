14e1d7e0f3b0a5663af1aa835983964d5dd9dcee
Axel Bolea <abolea@z1r4p3.42lyon.fr>
2024-06-07 12:39:31 +0200

    push pr faire test

--- srcs/parse_check_rl.c
+++ srcs/parse_check_rl.c
@@ -6,7 +6,7 @@
 /*   By: abolea <abolea@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 16:34:31 by abolea            #+#    #+#             */
-/*   Updated: 2024/06/05 17:16:03 by abolea           ###   ########.fr       */
+/*   Updated: 2024/06/07 12:33:09 by abolea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,6 @@ void	if_not_rl(t_data *da)
 	while (da->my_env[++i] != NULL)
 		free(da->my_env[i]);
 	free(da->my_env);
-}
-
-void	if_error_pipe(t_data *da)
-{
-	da->exit_status = 2;
-	da->parse_error = 1;
-	write(2, "too many pipes\n", 15);
 }
 
 void	if_error(t_data *da)
@@ -53,8 +46,6 @@ void	rl_ok(char *rl, t_data *da, char **envp)
 	}
 	if (rl[0])
 		add_history(rl);
-	if (nb_pipe(rl) == -1)
-		if_error_pipe(da);
 	if (rl && *rl != '\0' && da->parse_error != 1)
 		if_rl(rl, da, envp);
 	da->parse_error = 0;
